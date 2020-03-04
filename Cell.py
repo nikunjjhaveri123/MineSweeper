e6a783594ea40c1f92e41c717d21ddfbe8dcc3cf
Miraj Patel <mirajvpatel@gmail.com>
2020-03-04 16:13:27 -0500

    some more comments

--- Cell.py
+++ Cell.py
@@ -4,10 +4,10 @@ class Cell:
 
     def __init__(self, clue, identifiedSafes, hiddenSquares, identifiedMines, flagged, shown):
 
-        #the total number of mines surrounding this cell
+        #int: the total number of mines surrounding this cell
         self.clue = clue
 
-        #the number of identified safe cells surrounding this cell
+        #int: the number of identified safe cells surrounding this cell
         self.identifiedSafes = identifiedSafes
 
         #the number hidden square surrounding this cell (no. of unopened  cells)
