eb332cea7151097c774326e0a07d5b533fa49ec5
Miraj Patel <mirajvpatel@gmail.com>
2020-03-04 16:12:47 -0500

    commented the cell class

--- Cell.py
+++ Cell.py
@@ -2,10 +2,22 @@ import random
 
 class Cell:
 
-    def __init__(self, clue, detectedSafe, hiddenSpaces, detectedMines, flagged, shown):
+    def __init__(self, clue, identifiedSafes, hiddenSquares, identifiedMines, flagged, shown):
+
+        #the total number of mines surrounding this cell
         self.clue = clue
-        self.detectedSafe = detectedSafe
-        self.hiddenSpaces = hiddenSpaces
-        self.detectedMines = detectedMines
+
+        #the number of identified safe cells surrounding this cell
+        self.identifiedSafes = identifiedSafes
+
+        #the number hidden square surrounding this cell (no. of unopened  cells)
+        self.hiddenSquares = hiddenSquares
+
+        #int: number of mines identified
+        self.identifiedMines = identifieddMines
+
+        #boolean: if you flag this cell
         self.flagged = flagged
+
+        #boolean: whether this cell has been clicked on or not (we know if it is safe or not)
         self.shown = shown
